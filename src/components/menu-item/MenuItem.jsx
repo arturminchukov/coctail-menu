c1a5496f5ccefbec769f02cf904507436e600983
Artur Minchukov <aminchukov@yandex-team.ru>
2021-12-19 16:42:02 +0300

    Added more items in list

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,17 +1,17 @@
 import './MenuItem.css';
 
 export function MenuItem({name, structure, img, imgPos,extraInfo = null}) {
-    const posClass = imgPos === 'left' ? 'MenuItem__img_left' : 'MenuItem__img_right';
+    const imgPositionClass = imgPos === 'left' ? 'MenuItem__img_left' : 'MenuItem__img_right';
     return (
         <div className='MenuItem__container'>
             <div className='MenuItem__description'>
                 <h3 className='MenuItem__name'>{name}</h3>
                 <div className='MenuItem__structure'>
-                    {structure}
+                    {structure && structure.join(',')}
                 </div>
                 {extraInfo && <div className='MenuItem__extra-info'>*{extraInfo}</div>}
             </div>
-            <img className='MenuItem__img' src={img}/>
+            <img className={`MenuItem__img ${imgPositionClass}`} src={img}/>
         </div>
     );
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,15 @@
 import pinaColada from '../assets/coctails/pina-colada.png';
+import otvertka from '../assets/coctails/otvertka.png';
+import whiteRussian from '../assets/coctails/0954cd9b929e95675fc89ff22f4097c4-removebg-preview.png';
+import tequillaSunrise from '../assets/coctails/i-removebg-preview.png';
+import ginTonic from '../assets/coctails/gin-tonic-removebg-preview.png';
+import mohito from '../assets/coctails/mohito.png';
+import daikiri from '../assets/coctails/Daiquiri-Cocktail-Wallpaper-For-IPhone-removebg-preview.png';
+import margarita from '../assets/coctails/1455899697-removebg-preview.png';
+import blueLaguna from '../assets/coctails/00b23aee3cfd68cb183644e0fc67544f-removebg-preview.png';
+
+import karolJanRuby from '../assets/beers/ocr-removebg-preview.png';
+import alivariaBarhatnoe from '../assets/beers/i-removebg-preview.png';
 
 export const MENU = [
     {
@@ -6,10 +17,67 @@ export const MENU = [
         items: [
             {
                 name: 'Pina colada',
-                structure: ['rum','ananas juice'],
+                structure: ['ром', 'ананасовый сок', "сливки", "кокосовое молоко"],
                 img: pinaColada,
                 extraInfo: 'Доступен также в банановом и клубничной варианциях'
+            },
+            {
+                name: 'Отвертка',
+                structure: ['Водка', 'Сок'],
+                img: otvertka,
+                extraInfo: 'Сок вишневый, апельсиновый, ананасовый'
+            },{
+                name: 'Белый русский',
+                structure: ['Водка', 'кофейный ликер, сливки'],
+                img: whiteRussian,
+                extraInfo: ''
+            },{
+                name: 'Текилла санрайз',
+                structure: ['Текила, гренадин, апельсиновый сок'],
+                img: tequillaSunrise,
+                extraInfo: ''
+            },{
+                name: 'Джин тоник',
+                structure: ['Джин, тоник'],
+                img: ginTonic,
+                extraInfo: ''
+            },{
+                name: 'Мохито',
+                structure: ['Белый ром, спрайт, мята, сахарный сироп, лимон'],
+                img: mohito,
+                extraInfo: ''
+            },{
+                name: 'Дайкири',
+                structure: ['Белый ром, сахарный сироп, лимон'],
+                img: daikiri,
+                extraInfo: ''
+            },{
+                name: 'Маргарита',
+                structure: ['Текилла, трипл сек, сахарный сироп, лимон'],
+                img: margarita,
+                extraInfo: ''
+            },{
+                name: 'Голубая лагуна',
+                structure: ['Водка, кюрасау блю, спрайт'],
+                img: blueLaguna,
+                extraInfo: ''
             }
         ]
-    }
+    },
+    {
+        title: 'Beers',
+        items: [
+            {
+                name: 'Karol Jan Ruby',
+                structure: [],
+                img: karolJanRuby,
+                extraInfo: ''
+            },{
+                name: 'Лидское бархатное',
+                structure: [],
+                img: alivariaBarhatnoe,
+                extraInfo: ''
+            },
+        ]
+    },
 ];
